fcd7fbdc41a290f4e7445250e1de4e98c91b043f
abulubabah <rezamalikdrive@gmail.com>
2021-04-25 09:51:16 +0700

    database v1 without relation

--- estore.sql
+++ estore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 24, 2021 at 11:48 AM
+-- Generation Time: Apr 25, 2021 at 09:48 AM
 -- Server version: 10.3.27-MariaDB-0+deb10u1
 -- PHP Version: 7.3.27-9+0~20210227.82+debian10~1.gbpa4a3d6
 
@@ -24,6 +24,120 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(10) NOT NULL,
+  `category_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courier`
+--
+
+CREATE TABLE `courier` (
+  `courier_id` int(10) NOT NULL,
+  `courier_name` varchar(50) NOT NULL,
+  `courier_type` varchar(50) NOT NULL,
+  `courier_typePrice` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_id` int(10) NOT NULL,
+  `customer_name` varchar(50) NOT NULL,
+  `customer_gndr` int(1) NOT NULL,
+  `customer_address` text NOT NULL,
+  `customer_phone` varchar(15) NOT NULL,
+  `cusomer_email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `orders_id` int(10) NOT NULL,
+  `customer_id` int(10) NOT NULL,
+  `shipping_id` int(10) NOT NULL,
+  `courier_AWB` varchar(50) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ordersDetail`
+--
+
+CREATE TABLE `ordersDetail` (
+  `id` int(10) NOT NULL,
+  `orders_id` int(10) NOT NULL,
+  `product_id` int(10) NOT NULL,
+  `quantity` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(10) NOT NULL,
+  `product_sku` varchar(10) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `product_desc` text NOT NULL,
+  `product_price` int(20) NOT NULL,
+  `product_stock` int(10) NOT NULL,
+  `product_images` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productCategory`
+--
+
+CREATE TABLE `productCategory` (
+  `id` int(10) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipping`
+--
+
+CREATE TABLE `shipping` (
+  `shipping` int(10) NOT NULL,
+  `shipping_name` varchar(100) NOT NULL,
+  `shipping_country` varchar(50) NOT NULL,
+  `shipping_province` varchar(50) NOT NULL,
+  `shipping_city` varchar(50) NOT NULL,
+  `shipping_subDistrict` varchar(50) NOT NULL,
+  `shipping_urbVillage` varchar(50) NOT NULL,
+  `shipping_postalCode` int(10) NOT NULL,
+  `shipping_method` int(1) NOT NULL,
+  `courier_id` int(10) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `store`
 --
 
@@ -35,6 +149,19 @@ CREATE TABLE `store` (
   `store_phone` varchar(15) NOT NULL,
   `store_WANumber` varchar(15) NOT NULL,
   `store_TGNumber` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `transactions_id` int(10) NOT NULL,
+  `order_id` int(10) NOT NULL,
+  `transaction_image` text NOT NULL,
+  `status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -55,10 +182,64 @@ CREATE TABLE `user` (
 --
 
 --
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `courier`
+--
+ALTER TABLE `courier`
+  ADD PRIMARY KEY (`courier_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orders_id`);
+
+--
+-- Indexes for table `ordersDetail`
+--
+ALTER TABLE `ordersDetail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `productCategory`
+--
+ALTER TABLE `productCategory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `shipping`
+--
+ALTER TABLE `shipping`
+  ADD PRIMARY KEY (`shipping`);
+
+--
 -- Indexes for table `store`
 --
 ALTER TABLE `store`
   ADD PRIMARY KEY (`store_id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`transactions_id`);
 
 --
 -- Indexes for table `user`
@@ -69,6 +250,12 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `store`
